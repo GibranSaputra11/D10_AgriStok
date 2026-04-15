586e42dff98fff78287c46b3b9d290a70dbd24ac
Gibran Saputra <gibranbantul@gmail.com>
2026-04-15 23:42:06 +0700

    add logo dan icon

--- AgriStok/Form1.Designer.cs
+++ AgriStok/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.btnTani = new System.Windows.Forms.Button();
             this.btnIn = new System.Windows.Forms.Button();
             this.btnOut = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnBarang
@@ -86,11 +88,22 @@
             this.btnOut.UseVisualStyleBackColor = true;
             this.btnOut.Click += new System.EventHandler(this.btnOut_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AgriStok.Properties.Resources.AgristockLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(257, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(368, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(904, 501);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnOut);
             this.Controls.Add(this.btnIn);
             this.Controls.Add(this.btnTani);
@@ -100,6 +113,7 @@
             this.Name = "Dashboard";
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.Dashboard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -111,6 +125,7 @@
         private System.Windows.Forms.Button btnTani;
         private System.Windows.Forms.Button btnIn;
         private System.Windows.Forms.Button btnOut;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
