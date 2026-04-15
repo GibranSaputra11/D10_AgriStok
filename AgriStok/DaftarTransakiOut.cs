5c98164dfe31837bf71b4709a69deba762116c3d
Gibran Saputra <gibranbantul@gmail.com>
2026-04-15 23:17:44 +0700

    add method load data transaksi out

--- AgriStok/DaftarTransakiOut.cs
+++ AgriStok/DaftarTransakiOut.cs
@@ -22,7 +22,36 @@ namespace AgriStok
 
         private void DaftarTransakiOut_Load(object sender, EventArgs e)
         {
+            LoadMasterData();
+        }
 
+        private void LoadMasterData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"SELECT t.Id_Out AS [ID Transaksi], 
+                                            k.Nama_Kelompok AS [Nama Kelompok Tani], 
+                                            t.Tgl_Out AS [Tanggal Keluar], 
+                                            t.Total_Barang_Out AS [Total Item]
+                                     FROM Transaksi_Out t
+                                     INNER JOIN KelompokTani k ON t.Id_Kelompok = k.Id_Kelompok
+                                     ORDER BY t.Tgl_Out DESC";
+
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvMaster.DataSource = dt;
+
+                    dgvDetail.DataSource = null;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal memuat data Transaksi Keluar: " + ex.Message);
+                }
+            }
         }
     }
 }
