3544e91e668028f23d41d2660e30fa68faa21ae4
Gibran Saputra <gibranbantul@gmail.com>
2026-04-15 22:57:00 +0700

    add koneksi untuk form daftar transaksi

---
+++ AgriStok/DaftarTransaksiIn.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AgriStok
+{
+    partial class DaftarTransaksiIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvMaster = new System.Windows.Forms.DataGridView();
+            this.dgvDetail = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaster)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Daftar Transaksi In :";
+            // 
+            // dgvMaster
+            // 
+            this.dgvMaster.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMaster.Location = new System.Drawing.Point(37, 59);
+            this.dgvMaster.Name = "dgvMaster";
+            this.dgvMaster.Size = new System.Drawing.Size(342, 347);
+            this.dgvMaster.TabIndex = 1;
+            // 
+            // dgvDetail
+            // 
+            this.dgvDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetail.Location = new System.Drawing.Point(428, 59);
+            this.dgvDetail.Name = "dgvDetail";
+            this.dgvDetail.Size = new System.Drawing.Size(281, 266);
+            this.dgvDetail.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(425, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Detail Transaksi In :";
+            // 
+            // DaftarTransaksiIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvDetail);
+            this.Controls.Add(this.dgvMaster);
+            this.Controls.Add(this.label1);
+            this.Name = "DaftarTransaksiIn";
+            this.Text = "DaftarTransaksiIn";
+            this.Load += new System.EventHandler(this.DaftarTransaksiIn_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaster)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvMaster;
+        private System.Windows.Forms.DataGridView dgvDetail;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ AgriStok/DaftarTransaksiIn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace AgriStok
+{
+    public partial class DaftarTransaksiIn : Form
+    {
+        private readonly string connectionString = "Data Source=gibran-laptop;Initial Catalog=GudangPertanianDB;Integrated Security=True";
+        public DaftarTransaksiIn()
+        {
+            InitializeComponent();
+        }
+
+        private void DaftarTransaksiIn_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
