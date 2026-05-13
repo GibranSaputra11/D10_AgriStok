128c480efdc96f0a11ccafc334fa22320beca051
Gibran Saputra <gibranbantul@gmail.com>
2026-05-13 18:35:12 +0700

    updae bining dan view pada daftar transaski in

--- AgriStok/DaftarTransaksiIn.cs
+++ AgriStok/DaftarTransaksiIn.cs
@@ -15,6 +15,7 @@ namespace AgriStok
     public partial class DaftarTransaksiIn : Form
     {
         private readonly string connectionString = "Data Source=gibran-laptop;Initial Catalog=GudangPertanianDB;Integrated Security=True";
+
         public DaftarTransaksiIn()
         {
             InitializeComponent();
@@ -32,6 +33,8 @@ namespace AgriStok
             dgvDetail.ReadOnly = true;
             dgvDetail.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
 
+            bindingSourceMaster.PositionChanged += BindingSourceMaster_PositionChanged;
+
             LoadMasterData();
         }
 
@@ -41,26 +44,23 @@ namespace AgriStok
             {
                 try
                 {
-                    conn.Open();
-                    string query = @"SELECT t.Id_In AS [ID Transaksi], 
-                                            s.Nama_Supplier AS [Nama Supplier], 
-                                            t.Tgl_In AS [Tanggal Masuk], 
-                                            t.Total_Barang_In AS [Total Item]
-                                     FROM Transaksi_In t
-                                     INNER JOIN Supplier s ON t.Id_Supplier = s.Id_Supplier
-                                     ORDER BY t.Tgl_In DESC";
+                    string query = "SELECT * FROM vw_DaftarTransaksiIn ORDER BY [Tanggal Masuk] DESC";
 
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
                     DataTable dt = new DataTable();
                     da.Fill(dt);
-                    dgvMaster.DataSource = dt;
 
-                    dgvDetail.DataSource = null;
+                    bindingSourceMaster.DataSource = dt;
+                    dgvMaster.DataSource = bindingSourceMaster;
+
+                    if (bindingNavigatorMaster != null)
+                    {
+                        bindingNavigatorMaster.BindingSource = bindingSourceMaster;
+                    }
+
+                    BindingSourceMaster_PositionChanged(null, null);
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Gagal memuat data Transaksi Masuk: " + ex.Message);
-                }
+                catch (Exception ex) { MessageBox.Show("Gagal memuat data Master: " + ex.Message); }
             }
         }
 
@@ -70,13 +70,7 @@ namespace AgriStok
             {
                 try
                 {
-                    conn.Open();
-                    string query = @"SELECT b.Id_Barang AS [ID Barang], 
-                                            b.Nama_Barang AS [Nama Barang], 
-                                            d.Subtotal_In AS [Jumlah Masuk]
-                                     FROM Detail_In d
-                                     INNER JOIN Barang b ON d.Id_Barang = b.Id_Barang
-                                     WHERE d.Id_In = @Id";
+                    string query = "SELECT [ID Barang], [Nama Barang], [Jumlah Masuk] FROM vw_DetailTransaksiIn WHERE [ID Transaksi] = @Id";
 
                     SqlCommand cmd = new SqlCommand(query, conn);
                     cmd.Parameters.AddWithValue("@Id", idTransaksi);
@@ -84,7 +78,9 @@ namespace AgriStok
                     SqlDataAdapter da = new SqlDataAdapter(cmd);
                     DataTable dt = new DataTable();
                     da.Fill(dt);
+
                     dgvDetail.DataSource = dt;
+                                      
                 }
                 catch (Exception ex)
                 {
@@ -93,15 +89,20 @@ namespace AgriStok
             }
         }
 
-        private void dgvMaster_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        
+        private void BindingSourceMaster_PositionChanged(object sender, EventArgs e)
         {
-            if (e.RowIndex >= 0)
+            if (bindingSourceMaster.Current != null)
             {
-                DataGridViewRow row = dgvMaster.Rows[e.RowIndex];
+                DataRowView currentRow = (DataRowView)bindingSourceMaster.Current;
 
-                string idTransaksi = row.Cells["ID Transaksi"].Value.ToString();
+                string idTransaksi = currentRow["ID Transaksi"].ToString();
 
                 LoadDetailData(idTransaksi);
+            }
+            else
+            {
+                dgvDetail.DataSource = null;
             }
         }
 
