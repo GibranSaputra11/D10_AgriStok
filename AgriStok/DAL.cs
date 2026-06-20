52f1dc20f1e3488834fd663f05a5b7ee0d63c127
Gibran Saputra <gibranbantul@gmail.com>
2026-06-20 19:07:37 +0700

    setup DAL dan konetivitas

---
+++ AgriStok/DAL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgriStok
+{
+    internal class DAL
+    {
+        private readonly string connectionString;
+
+        public DAL()
+        {
+            connectionString = GetConnectionString();
+        }
+
+        public string GetConnectionString()
+        {
+            string localIP = GetLocalIPAddress();
+            return $"Data Source={localIP};Initial Catalog=GudangPertanianDB;User ID=sa;Password=12345678;";
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            string localIP = string.Empty;
+            try
+            {
+                var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+                foreach (var ip in host.AddressList)
+                {
+                    if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        localIP = ip.ToString();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error getting local IP address: " + ex.Message);
+            }
+            return localIP;
+        }
+
+        
+    }
+}
