de3e51962e5e197157b1d688d4fc2892fe0233f6
Gibran Saputra <gibranbantul@gmail.com>
2026-06-23 12:04:37 +0700

    add form reort

---
+++ AgriStok/ReportIn.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgriStok
+{
+    public partial class ReportIn : Form
+    {
+        private DAL dbLogic = new DAL();
+        private string idTransaksi;
+
+        public ReportIn(string id)
+        {
+            InitializeComponent();
+            idTransaksi = id;
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dtNota = dbLogic.GetNotaTransaksiIn(idTransaksi);
+
+                CrystalReportIn laporanNota = new CrystalReportIn();
+
+                laporanNota.SetDataSource(dtNota);
+
+                crystalReportViewer1.ReportSource = laporanNota;
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal memuat Nota: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
