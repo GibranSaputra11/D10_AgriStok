a0bc7220d0a4396a52936798b58200d543005812
Gibran Saputra <gibranbantul@gmail.com>
2026-06-23 12:04:13 +0700

    add class data in

---
+++ AgriStok/DataNotaIn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgriStok
+{
+    internal class DataNotaIn
+    {
+        public string ID_Transaksi { get; set; }
+        public string Nama_Supplier { get; set; }
+        public DateTime Tanggal { get; set; }
+        public string Nama_Barang { get; set; }
+        public int Jumlah { get; set; }
+    }
+}
