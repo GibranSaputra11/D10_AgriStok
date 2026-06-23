2a9ad7f6eaad9ef1bd60ae86cb64438da8ab2244
Gibran Saputra <gibranbantul@gmail.com>
2026-06-23 14:07:42 +0700

    add class  dara report tr out

---
+++ AgriStok/DataNotaOut.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgriStok
+{
+    internal class DataNotaOut
+    {
+        public string ID_Transaksi { get; set; }
+        public string Nama_Kelompok { get; set; } 
+        public DateTime Tanggal { get; set; }
+        public string Nama_Barang { get; set; }
+        public int Jumlah { get; set; }
+    }
+}
