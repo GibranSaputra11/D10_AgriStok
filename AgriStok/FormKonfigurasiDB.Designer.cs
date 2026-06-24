f0e1748d8b52023e22b32dbdcf87baf003a7061f
Gibran Saputra <gibranbantul@gmail.com>
2026-06-24 22:45:52 +0700

    add form konfigurasidb

---
+++ AgriStok/FormKonfigurasiDB.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace AgriStok
+{
+    partial class FormKonfigurasiDB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnGetIP = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(151, 106);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(259, 22);
+            this.txtServer.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Server";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(151, 195);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(259, 22);
+            this.txtUser.TabIndex = 2;
+            this.txtUser.Text = "sa";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(151, 236);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(259, 22);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "User";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.BackColor = System.Drawing.Color.Transparent;
+            this.btnSimpan.Location = new System.Drawing.Point(151, 277);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(177, 36);
+            this.btnSimpan.TabIndex = 6;
+            this.btnSimpan.Text = "Simpan dan Lanjutkan";
+            this.btnSimpan.UseVisualStyleBackColor = false;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnGetIP
+            // 
+            this.btnGetIP.BackColor = System.Drawing.Color.Transparent;
+            this.btnGetIP.Location = new System.Drawing.Point(151, 146);
+            this.btnGetIP.Name = "btnGetIP";
+            this.btnGetIP.Size = new System.Drawing.Size(96, 32);
+            this.btnGetIP.TabIndex = 7;
+            this.btnGetIP.Text = "Get IP";
+            this.btnGetIP.UseVisualStyleBackColor = false;
+            this.btnGetIP.Click += new System.EventHandler(this.btnGetIP_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(75, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(240, 26);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Konfigurasi Database";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(253, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 32);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Get Server Name";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnGetServerLokal_Click);
+            // 
+            // FormKonfigurasiDB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 441);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnGetIP);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtServer);
+            this.Name = "FormKonfigurasiDB";
+            this.Text = "FormKonfigurasiDB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnGetIP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ AgriStok/FormKonfigurasiDB.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace AgriStok
+{
+    public partial class FormKonfigurasiDB : Form
+    {
+        public FormKonfigurasiDB()
+        {
+            InitializeComponent();
+
+            try
+            {
+                if (ConfigurationManager.ConnectionStrings["GudangDbConn"] != null)
+                {
+                    string connStringLama = ConfigurationManager.ConnectionStrings["GudangDbConn"].ConnectionString;
+
+                    if (!string.IsNullOrEmpty(connStringLama))
+                    {
+                        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connStringLama);
+
+                        txtServer.Text = builder.DataSource;
+                        txtUser.Text = builder.UserID;
+                        txtPassword.Text = builder.Password;
+                    }
+                }
+                else
+                {
+                    txtUser.Text = "sa"; 
+                }
+            }
+            catch { }
+        }
+
+       
+        private void btnGetIP_Click(object sender, EventArgs e)
+        {
+            txtServer.Text = GetLocalIPAddress();
+        }
+
+        private string GetLocalIPAddress()
+        {
+            string localIP = string.Empty;
+            try
+            {
+                foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    if (item.OperationalStatus == OperationalStatus.Up &&
+                        (item.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 ||
+                         item.NetworkInterfaceType == NetworkInterfaceType.Ethernet))
+                    {
+                        string deskripsi = item.Description.ToLower();
+                        if (deskripsi.Contains("vmware") || deskripsi.Contains("virtual") || deskripsi.Contains("pseudo"))
+                        {
+                            continue; 
+                        }
+
+                        var gateways = item.GetIPProperties().GatewayAddresses;
+                        if (gateways.Count == 0)
+                        {
+                            continue; 
+                        }
+
+                        foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                            {
+                                localIP = ip.Address.ToString();
+                                return localIP;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error mendeteksi IP Wi-Fi/LAN: " + ex.Message, "Error Jaringan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            if (string.IsNullOrEmpty(localIP))
+            {
+                localIP = "127.0.0.1"; 
+            }
+
+            return localIP;
+        }
+
+        private void btnGetServerLokal_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string serverDitemukan = ".";
+
+                RegistryKey baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                RegistryKey key = baseKey.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL");
+
+                if (key != null && key.GetValueNames().Length > 0)
+                {
+                    string namaInstance = key.GetValueNames()[0];
+
+                    if (namaInstance != "MSSQLSERVER")
+                    {
+                        serverDitemukan = $@".\{namaInstance}";
+                    }
+                }
+
+                txtServer.Text = serverDitemukan;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal mendeteksi server SQL lokal.\nDetail: " + ex.Message, "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtServer.Text = ".";
+            }
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            string ipServer = txtServer.Text;
+            string userDb = txtUser.Text;
+            string passDb = txtPassword.Text;
+
+            string koneksiBaru = $"Data Source={ipServer};Initial Catalog=GudangPertanianDB;User ID={userDb};Password={passDb};";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(koneksiBaru))
+                {
+                    conn.Open();
+                }
+
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                if (config.ConnectionStrings.ConnectionStrings["GudangDbConn"] != null)
+                {
+                    config.ConnectionStrings.ConnectionStrings["GudangDbConn"].ConnectionString = koneksiBaru;
+                }
+                else
+                {
+                    config.ConnectionStrings.ConnectionStrings.Add(new ConnectionStringSettings("GudangDbConn", koneksiBaru, "System.Data.SqlClient"));
+                }
+
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("connectionStrings");
+
+                MessageBox.Show("Konfigurasi Database berhasil disimpan! Aplikasi siap digunakan.", "Koneksi Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                this.Hide();
+                Dashboard formUtama = new Dashboard();
+                formUtama.ShowDialog();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi Gagal! Pastikan IP dan Password benar, serta SQL Server/Firewall sudah diizinkan.\n\nDetail: " + ex.Message, "Error Koneksi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
