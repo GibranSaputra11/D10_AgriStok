852b70d718390c8bd5bb95019da39e28d4a19672
Gibran Saputra <gibranbantul@gmail.com>
2026-06-25 20:56:17 +0700

    updtae contrain barang

--- AgriStok/KelolaBarang.cs
+++ AgriStok/KelolaBarang.cs
@@ -98,6 +98,69 @@ namespace AgriStok
                 return;
             }
 
+            string namaBarangInput = txtNamaBarang.Text.ToLower();
+
+            List<string> whitelistPertanian = new List<string>
+            {
+                "benih", "bibit", "biji", "stek", "cangkok", "okulasi", "kultur jaringan", "spora",
+
+                "padi", "jagung", "kedelai", "kacang hijau", "kacang tanah", "kacang panjang", "gandum",
+                "sorgum", "singkong", "ubi", "ketela", "porang", "talas", "garut", "edamame",
+
+                "kubis", "sawi", "bayam", "kangkung", "selada", "pakcoy", "caisim", "seledri",
+                "daun bawang", "brokoli", "kembang kol", "kemangi", "kelor", "asparagus", "rebung",
+
+                "cabai", "lombok", "tomat", "bawang merah", "bawang putih", "bawang bombay",
+                "kentang", "wortel", "terong", "pare", "labu", "oyong", "gambas", "paprika",
+                "buncis", "kecipir", "lobak", "bit",
+
+                "melon", "semangka", "mangga", "jeruk", "apel", "alpukat", "durian", "rambutan",
+                "manggis", "salak", "pepaya", "pisang", "nanas", "buah naga", "stroberi", "anggur",
+                "kelengkeng", "duku", "belimbing", "jambu", "nangka", "sirsak", "markisa", "delima",
+
+                "kelapa sawit", "kelapa", "karet", "kakao", "kopi", "tebu", "tembakau", "cengkeh",
+                "teh", "lada", "pala", "kemiri", "vanili", "kapas", "nilam", "sereh wangi", "aren",
+
+                "jahe", "kunyit", "lengkuas", "laos", "kencur", "temulawak", "kapulaga",
+                "ketumbar", "merica", "kayu manis", "sereh", "lidah buaya", "mahkota dewa",
+
+                "jati", "sengon", "mahoni", "gaharu", "albasia", "akasia", "balsa", "jabon", "bambu",
+                "pupuk", "urea", "npk", "za", "kcl", "tsp", "sp-36", "sp36", "phonska",
+                "mutiara", "nitrat", "kalium", "fosfat", "magnesium", "kalsium", "boron",
+
+                "kompos", "kandang", "poc", "organik", "hayati", "mikoriza", "rhizobium",
+                "guano", "humat", "asam amino", "ekoenzim", "bokashi", "trichoderma",
+
+                "pestisida", "insektisida", "herbisida", "fungisida", "nematisida",
+                "rodentisida", "bakterisida", "akarisida", "moluskisida", "furadan",
+                "roundup", "gramoxone", "antracol", "dithane", "regent", "abamektin",
+                "glifosat", "paraquat", "mankozeb", "perekat", "penembus", "perata",
+
+                "cangkul", "sabit", "parang", "arit", "garu", "bajak", "sekop", "gembor",
+                "gunting dahan", "gergaji", "golok", "linggis", "dodos", "egrek",
+
+                "traktor", "mesin", "pompa air", "sprayer", "combine harvester",
+                "transplanter", "thresher", "cultivator", "rotavator", "genset",
+                "chopper", "dryer", "seeder", "mist blower", "fogger",
+
+                "mulsa", "polybag", "paranet", "shading net", "waring", "terpal",
+                "tali salaran", "ajir", "bambu", "cocopeat", "sekam", "rockwool",
+                "hidroponik", "netpot", "ab mix", "selang drip", "emiter", "ph meter",
+                "tds meter", "hygrometer", "thermometer",
+
+                "drone", "sensor", "iot", "mikrokontroler", "aktuator", "smart pad",
+                "rfid", "barcode", "gps", "cuaca", "telemetri", "otomatisasi"
+            };
+
+            bool isSesuaiWhitelist = whitelistPertanian.Any(keyword => namaBarangInput.Contains(keyword));
+
+            if (!isSesuaiWhitelist)
+            {
+                MessageBox.Show("Barang ditolak!\nSistem mendeteksi barang ini tidak termasuk dalam kategori atau inventaris pertanian.\n\nPastikan nama barang mengandung kata kunci pertanian yang valid (Contoh: 'Pupuk', 'Benih', 'Traktor', dll).",
+                                "Di Luar Konteks", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             try
             {
                 byte[] imgBytes = ConvertImageToBytes(pbFoto);
@@ -183,6 +246,81 @@ namespace AgriStok
             if (txtBarangID.Text == dbLogic.GenerateIdBarang())
             {
                 MessageBox.Show("Pilih data yang sudah ada di tabel terlebih dahulu untuk diubah!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtNamaBarang.Text) || txtNamaBarang.Text.Length < 3)
+            {
+                MessageBox.Show("Nama tidak boleh terlalu pendek!\nMasukkan minimal 3 karakter.", "Validasi Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!Regex.IsMatch(txtNamaBarang.Text, @"^[a-zA-Z0-9\s\.,]+$"))
+            {
+                MessageBox.Show("Nama mengandung simbol yang tidak diizinkan!\nHanya gunakan huruf, angka, spasi, titik (.), atau koma (,).", "Validasi Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string namaBarangInput = txtNamaBarang.Text.ToLower();
+
+            List<string> whitelistPertanian = new List<string>
+            {
+                "benih", "bibit", "biji", "stek", "cangkok", "okulasi", "kultur jaringan", "spora",
+
+                "padi", "jagung", "kedelai", "kacang hijau", "kacang tanah", "kacang panjang", "gandum",
+                "sorgum", "singkong", "ubi", "ketela", "porang", "talas", "garut", "edamame",
+
+                "kubis", "sawi", "bayam", "kangkung", "selada", "pakcoy", "caisim", "seledri",
+                "daun bawang", "brokoli", "kembang kol", "kemangi", "kelor", "asparagus", "rebung",
+
+                "cabai", "lombok", "tomat", "bawang merah", "bawang putih", "bawang bombay",
+                "kentang", "wortel", "terong", "pare", "labu", "oyong", "gambas", "paprika",
+                "buncis", "kecipir", "lobak", "bit",
+
+                "melon", "semangka", "mangga", "jeruk", "apel", "alpukat", "durian", "rambutan",
+                "manggis", "salak", "pepaya", "pisang", "nanas", "buah naga", "stroberi", "anggur",
+                "kelengkeng", "duku", "belimbing", "jambu", "nangka", "sirsak", "markisa", "delima",
+
+                "kelapa sawit", "kelapa", "karet", "kakao", "kopi", "tebu", "tembakau", "cengkeh",
+                "teh", "lada", "pala", "kemiri", "vanili", "kapas", "nilam", "sereh wangi", "aren",
+
+                "jahe", "kunyit", "lengkuas", "laos", "kencur", "temulawak", "kapulaga",
+                "ketumbar", "merica", "kayu manis", "sereh", "lidah buaya", "mahkota dewa",
+
+                "jati", "sengon", "mahoni", "gaharu", "albasia", "akasia", "balsa", "jabon", "bambu",
+                "pupuk", "urea", "npk", "za", "kcl", "tsp", "sp-36", "sp36", "phonska",
+                "mutiara", "nitrat", "kalium", "fosfat", "magnesium", "kalsium", "boron",
+
+                "kompos", "kandang", "poc", "organik", "hayati", "mikoriza", "rhizobium",
+                "guano", "humat", "asam amino", "ekoenzim", "bokashi", "trichoderma",
+
+                "pestisida", "insektisida", "herbisida", "fungisida", "nematisida",
+                "rodentisida", "bakterisida", "akarisida", "moluskisida", "furadan",
+                "roundup", "gramoxone", "antracol", "dithane", "regent", "abamektin",
+                "glifosat", "paraquat", "mankozeb", "perekat", "penembus", "perata",
+
+                "cangkul", "sabit", "parang", "arit", "garu", "bajak", "sekop", "gembor",
+                "gunting dahan", "gergaji", "golok", "linggis", "dodos", "egrek",
+
+                "traktor", "mesin", "pompa air", "sprayer", "combine harvester",
+                "transplanter", "thresher", "cultivator", "rotavator", "genset",
+                "chopper", "dryer", "seeder", "mist blower", "fogger",
+
+                "mulsa", "polybag", "paranet", "shading net", "waring", "terpal",
+                "tali salaran", "ajir", "bambu", "cocopeat", "sekam", "rockwool",
+                "hidroponik", "netpot", "ab mix", "selang drip", "emiter", "ph meter",
+                "tds meter", "hygrometer", "thermometer",
+
+                "drone", "sensor", "iot", "mikrokontroler", "aktuator", "smart pad",
+                "rfid", "barcode", "gps", "cuaca", "telemetri", "otomatisasi"
+            };
+
+            bool isSesuaiWhitelist = whitelistPertanian.Any(keyword => namaBarangInput.Contains(keyword));
+
+            if (!isSesuaiWhitelist)
+            {
+                MessageBox.Show("Barang ditolak!\nSistem mendeteksi barang ini tidak termasuk dalam kategori atau inventaris pertanian.\n\nPastikan nama barang mengandung kata kunci pertanian yang valid (Contoh: 'Pupuk', 'Benih', 'Traktor', dll).",
+                                "Di Luar Konteks", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
